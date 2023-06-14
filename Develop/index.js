95a40c70be971fda46ae149a7dd2a25222f9f616
mvfranzke <michellevonfranzke@gmail.com>
2023-06-14 16:16:16 -0700

    import modules fs and inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,5 @@
-// TODO: Include packages needed for this application
+//import the modules fs and inquirer
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
